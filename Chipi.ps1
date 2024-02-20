e1674a24f3899251ab7be901229b2ac65732ebaf
MrDrHax <alexfh2001@gmail.com>
2024-02-20 11:00:49 -0600

    first commit

---
+++ Chipi.ps1
@@ -0,0 +1,64 @@
+Add-Type -TypeDefinition @'
+using System.Runtime.InteropServices;
+
+[Guid("5CDF2C82-841E-4546-9722-0CF74078229A"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+interface IAudioEndpointVolume {
+    int f(); int g(); int h(); int i();
+    int SetMasterVolumeLevelScalar(float fLevel, System.Guid pguidEventContext);
+    int j();
+    int GetMasterVolumeLevelScalar(out float pfLevel);
+    int SetMute([MarshalAs(UnmanagedType.Bool)] bool isMuted, ref Guid eventContext);
+    int GetMute(out bool isMuted);
+}
+
+[Guid("D666063F-1587-4E43-81F1-B948E807363F"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+interface IMMDevice {
+    int Activate(ref System.Guid id, int clsCtx, int activationParams, out IAudioEndpointVolume aev);
+}
+
+[Guid("A95664D2-9614-4F35-A746-DE8DB63617E6"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+interface IMMDeviceEnumerator {
+    int f(); // Unused
+    int GetDefaultAudioEndpoint(int dataFlow, int role, out IMMDevice endpoint);
+}
+
+[ComImport, Guid("BCDE0395-E52F-467C-8E3D-C4579291692E")]
+class MMDeviceEnumeratorComObject { }
+
+public class Audio {
+    static IAudioEndpointVolume Vol() {
+        var enumerator = new MMDeviceEnumeratorComObject() as IMMDeviceEnumerator;
+        IMMDevice dev = null;
+        Marshal.ThrowExceptionForHR(enumerator.GetDefaultAudioEndpoint(/*eRender*/ 0, /*eMultimedia*/ 1, out dev));
+        IAudioEndpointVolume epv = null;
+        var epvid = typeof(IAudioEndpointVolume).GUID;
+        Marshal.ThrowExceptionForHR(dev.Activate(ref epvid, /*CLSCTX_ALL*/ 23, 0, out epv));
+        return epv;
+    }
+    public static float Volume {
+        get {float v = -1; Marshal.ThrowExceptionForHR(Vol().GetMasterVolumeLevelScalar(out v)); return v;}
+        set {Marshal.ThrowExceptionForHR(Vol().SetMasterVolumeLevelScalar(value, System.Guid.Empty));}
+    }
+    public static bool Mute {
+        get {
+            bool mute;
+            Marshal.ThrowExceptionForHR(Vol().GetMute(out mute));
+            return mute;
+        }
+        set {
+            Marshal.ThrowExceptionForHR(Vol().SetMute(value, Guid.Empty));
+        }
+    }
+}
+'@
+
+while (1)
+{
+    # remove mute and set volume to 50%
+    [Audio]::Mute = $false
+    [Audio]::Volume = 0.5
+    # open a random youtube video
+    Start-Process "https://www.youtube.com/watch?v=xbks1QDEWNM"
+    # wait for anywhere between 1 minute and 30 minutes
+    Start-Sleep -Seconds (Get-Random -Minimum 60 -Maximum 1800)
+}
---
+++ readme.md
@@ -0,0 +1,11 @@
+# THIS PROJECT IS DEFINITELY NOT A VIRUS
+
+You can tell because it actually has no install script.
+
+This project is actually a supper helpful tool.
+
+It was created for meme addicts, so that they keep just the right amount of dopamine. 
+
+So IT'S NOT A VIRUS.
+
+Ok?
